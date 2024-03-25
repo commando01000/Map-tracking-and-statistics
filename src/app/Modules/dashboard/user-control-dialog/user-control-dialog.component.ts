2af2486e48b8ea9808149471ed553dc89b33a937
Mahmoud Ibrahim <jfijcc124@gmail.com>
2024-03-25 11:18:30 +0200

    add photo done

--- src/app/Modules/dashboard/user-control-dialog/user-control-dialog.component.ts
+++ src/app/Modules/dashboard/user-control-dialog/user-control-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'user-control-dialog',
@@ -12,17 +12,20 @@ export class UserControlDialogComponent {
 
   ngOnInit(): void {}
 
-  displaySelectedImage(event: any, elementId: any) {
-    const selectedImage: any = document.getElementById(elementId);
+  @ViewChild('selectedImage', { static: false }) selectedImage!: ElementRef<HTMLImageElement>;
+
+  displaySelectedImage(event:any) {
     const fileInput = event.target;
-  
+    
     if (fileInput.files && fileInput.files[0]) {
       const reader = new FileReader();
-  
-      reader.onload = function (e: any) {
-        selectedImage.src = e.target.result;
+
+      reader.onload = (e: any) => {
+        console.log(e.target.result);
+        
+        this.selectedImage.nativeElement.src = e.target.result;
       };
-  
+
       reader.readAsDataURL(fileInput.files[0]);
     }
   }
